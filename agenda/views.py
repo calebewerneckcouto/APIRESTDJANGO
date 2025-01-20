b9ec715d29dc53f7a620b7c8cb172c0ce209d7ed
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-20 19:47:50 -0300

    Utilizando o decorator api_view

--- agenda/views.py
+++ agenda/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 from django.shortcuts import get_object_or_404
 from agenda.models import Agendamento
@@ -6,11 +7,13 @@ from agenda.serializers import AgendamentoSerializer
 from django.http import JsonResponse
 
 # Create your views here.
+@api_view(http_method_names=["GET"])
 def agendamento_detail(request,id):
     obj=get_object_or_404(Agendamento,id=id)
     serializer =AgendamentoSerializer(obj)
     return JsonResponse(serializer)
 
+@api_view(http_method_names=["GET"])
 def agendamento_list(request):
     qs = Agendamento.objects.all()
     serializer =AgendamentoSerializer(qs,many=True)
