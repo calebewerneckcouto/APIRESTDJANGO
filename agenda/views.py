3e037c00adb685c81ab829f750d805d71a6d05bc
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-20 20:02:32 -0300

    Criando um agendamento

--- agenda/views.py
+++ agenda/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+
 from rest_framework.decorators import api_view
 
 from django.shortcuts import get_object_or_404
@@ -13,8 +13,22 @@ def agendamento_detail(request,id):
     serializer =AgendamentoSerializer(obj)
     return JsonResponse(serializer)
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET","POST"])
 def agendamento_list(request):
-    qs = Agendamento.objects.all()
-    serializer =AgendamentoSerializer(qs,many=True)
-    return JsonResponse(serializer.data,safe=False)
+    if request.method == "GET":
+        qs = Agendamento.objects.all()
+        serializer =AgendamentoSerializer(qs,many=True)
+        return JsonResponse(serializer.data,safe=False)
+    if request.method == "POST":
+        data = request.data
+        serializer =AgendamentoSerializer(data=data)
+        if serializer.is_valid():
+            validated_data= serializer.validated_data
+            Agendamento.objects.create(
+                data_horario=validated_data["data_horario"],
+                nome_cliente=validated_data["nome_cliente"],
+                email_cliente=validated_data["email_cliente"],
+                telefone_cliente=validated_data["telefone_cliente"],
+            )
+            return JsonResponse(serializer.data,status=201)
+        return JsonResponse(serializer.error,status=400)
